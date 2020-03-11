0e1c826e0ce580895eba63b7a17a02e22ffd38f8
Andrej Safranovic <viscolex@hotmail.com>
2020-03-11 23:17:48 +0000

    Fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
     ],
 
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Raleway:700,800&display=swap' }
     ]
   },
   /*
