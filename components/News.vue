618d9172c851c9a1a5cd48931f9ad3476648e727
Andrej Safranovic <viscolex@hotmail.com>
2020-03-17 15:32:31 +0000

    Fix news order

--- components/News.vue
+++ components/News.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="news-container" v-show="allNews != null || undefined">
+  <div id="news-container" v-show="txbitnewsposts != null || undefined">
     <div class="uk-grid uk-grid-match hide-scrollbar">
-      <div v-for="newspost in allNews" :key="newspost.id" style="width:100%">
-        <div
-          class="uk-card uk-margin-bottom container-fluid card-background uk-box-shadow-small "
-        >
+      <div
+        v-for="newspost in txbitnewsposts.reverse().slice(0, 10)"
+        :key="newspost.id"
+        style="width:100%"
+      >
+        <div class="uk-card uk-margin-bottom container-fluid card-background uk-box-shadow-small">
           <div class="p-2">
             <div class="row">
               <div
@@ -26,16 +28,19 @@
               </div>
             </div>
             <div class="row">
-              <span id="content" v-show="newspost.content">{{
+              <span id="content" v-show="newspost.content">
+                {{
                 newspost.content
-              }}</span>
+                }}
+              </span>
             </div>
             <div class="row">
               <div class="uk-clearfix">
                 <div class="uk-float-right">
-                  <span id="date" v-show="newspost.published_at">
-                    {{ moment(newspost.published_at).format("Do MMM YYYY") }}
-                  </span>
+                  <span
+                    id="date"
+                    v-show="newspost.published_at"
+                  >{{ moment(newspost.published_at).format("Do MMM YYYY") }}</span>
                 </div>
               </div>
             </div>
@@ -64,11 +69,6 @@ export default {
     txbitnewsposts: {
       prefetch: true,
       query: txbitnewspostsQuery
-    }
-  },
-  computed: {
-    allNews() {
-      return this.txbitnewsposts.reverse().slice(0, 10);
     }
   }
 };
